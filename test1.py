84f7c28a5f4f298c2349a01cc84f272324d629b5
Nathan Sagnes <nathan.sagnes@etu.minesparis.psl.eu>
2026-09-08 10:29:25 +0200

    partie2

--- test1.py
+++ test1.py
@@ -1,6 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import random as rd
 
-image =np.empty((91,91,3),dtype=np.uint8)
+im = plt.imread('data/les-mines.jpg') #on lit le fichier image
+im.flags.writeable
+
+
+image =np.empty((91,91,3),dtype=np.uint8) #on crée une image normalisée
+#image[:] = [0,255,0]
+#print(image[0,0],image[-1,-1])
+image[::10] = [0,0,255] # on trace les lignes en bleu
+image[:,::10] = [0,0,255] #on trace les colonnes en bleu
+
 plt.imshow(image)
-plt.show()
+#plt.show()
