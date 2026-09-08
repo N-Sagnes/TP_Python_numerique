4a85ec218ed7ce53829a16a3ab8b6a6b7a3a3db4
Nathan Sagnes <nathan.sagnes@etu.minesparis.psl.eu>
2026-09-08 10:39:30 +0200

    question-image

--- test1.py
+++ test1.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import random as rd
 
 im = plt.imread('data/les-mines.jpg') #on lit le fichier image
-im.flags.writeable
-
+im2 = im.copy()
+print(bool(im2.flags.writeable)) #on vérifie si l'image est modifiable
+print(type(im2)) #on vérifie le type de l'objet
 
 image =np.empty((91,91,3),dtype=np.uint8) #on crée une image normalisée
 #image[:] = [0,255,0]
@@ -12,5 +13,6 @@ image =np.empty((91,91,3),dtype=np.uint8) #on crée une image normalisée
 image[::10] = [0,0,255] # on trace les lignes en bleu
 image[:,::10] = [0,0,255] #on trace les colonnes en bleu
 
-plt.imshow(image)
+#plt.imshow(image)# on affiche l'image colorée
+plt.imshow(im) #on affiche l'image des mines
 #plt.show()
